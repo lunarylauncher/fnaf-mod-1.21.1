3092a530f462c0c065cc128a2eb51afc3d6963d6
MrRitess <sirrites@gmail.com>
2025-10-09 02:23:03 +1000

    1

--- src/main/java/net/mrrites/fnafmod/FNAFMOD.java
+++ src/main/java/net/mrrites/fnafmod/FNAFMOD.java
@@ -1,30 +1,7 @@
-//package net.mrrites.fnafmod;
-//
-//import net.fabricmc.api.ModInitializer;
-//
-//import net.mrrites.fnafmod.block.ModBlocks;
-//import net.mrrites.fnafmod.block.entity.ModBlockEntity;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//public class FNAFMOD implements ModInitializer {
-//	public static final String MOD_ID = "fnafmod";
-//
-//	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-//
-//	@Override
-//	public void onInitialize() {
-//		ModBlocks.registerModBlocks();
-//		ModBlockEntity.registerBlockEntities();
-//		LOGGER.info("Hello Fabric world!");
-//
-//	}
-//}
-
 package net.mrrites.fnafmod;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
 import net.mrrites.fnafmod.block.ModBlocks;
 import net.mrrites.fnafmod.block.entity.ModBlockEntity;
 import org.slf4j.Logger;
@@ -32,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class FNAFMOD implements ModInitializer {
 	public static final String MOD_ID = "fnafmod";
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -40,9 +18,5 @@ public class FNAFMOD implements ModInitializer {
 		ModBlockEntity.registerBlockEntities();
 		LOGGER.info("Hello Fabric world!");
 
-		// Регистрация команды для управления шейдерами
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			ShaderAdminManager.registerCommand(dispatcher);
-		});
 	}
 }
--- src/main/java/net/mrrites/fnafmod/FNAFMODClient.java
+++ src/main/java/net/mrrites/fnafmod/FNAFMODClient.java
@@ -1,42 +1,12 @@
-//package net.mrrites.fnafmod;
-//
-//import net.fabricmc.api.ClientModInitializer;
-//
-//
-//public class FNAFMODClient implements ClientModInitializer {
-//    @Override
-//    public void onInitializeClient() {
-//
-//
-//    }
-//}
 package net.mrrites.fnafmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+
 
 public class FNAFMODClient implements ClientModInitializer {
-    private static final Map<UUID, Boolean> previousShaderState = new HashMap<>();
-
     @Override
     public void onInitializeClient() {
-        ShaderPayload.registerClient((client, enable) -> {
-            UUID playerId = client.player.getUuid();
-            client.execute(() -> {
-                var config = net.irisshaders.iris.api.v0.IrisApi.getInstance().getConfig();
-                if (enable) {
-                    Boolean prevEnabled = previousShaderState.get(playerId);
-                    if (prevEnabled != null && prevEnabled) {
-                        config.setShadersEnabledAndApply(true);
-                    }
-                } else {
-                    previousShaderState.put(playerId, config.areShadersEnabled());
-                    config.setShadersEnabledAndApply(false);
-                }
-            });
-        });
+
+
     }
 }
